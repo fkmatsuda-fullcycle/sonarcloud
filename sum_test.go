95dbbee117d650da3368213fcc440575700d8d29
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2022-03-23 23:45:25 -0300

    ci: add more tests

--- sum_test.go
+++ sum_test.go
@@ -8,3 +8,14 @@ func TestSum(t *testing.T) {
 		t.Errorf("Sum(%d, %d) = %d, want %d", a, b, sum(a, b), 3)
 	}
 }
+
+func TestProcessArgs(t *testing.T) {
+	a, b := "2", "3"
+	result, err := processArgs([]string{"sum", a, b})
+	if err != nil {
+		t.Error(err)
+	}
+	if result != "2 + 3 = 5" {
+		t.Errorf("processArgs(%s, %s) = %s, want %s", a, b, result, "2 + 3 = 5")
+	}
+}
